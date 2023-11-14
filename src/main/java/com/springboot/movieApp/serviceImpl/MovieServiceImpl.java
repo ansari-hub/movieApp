c9c5a445d59c1b3c576b41a538efaa087ba7f5eb
Ansari Shaik <javeed.ansari.shaik@accenture.com>
2023-11-14 11:37:32 +0530

    Service layer logic

---
+++ src/main/java/com/springboot/movieApp/serviceImpl/MovieServiceImpl.java
@@ -0,0 +1,77 @@
+package com.springboot.movieApp.serviceImpl;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.springboot.movieApp.Entity.Movie;
+import com.springboot.movieApp.dao.MovieDao;
+import com.springboot.movieApp.service.MovieService;
+
+public class MovieServiceImpl implements MovieService{
+		@Autowired
+		private MovieDao movieDao;
+		private Movie movie;
+		@Override
+		public Movie getMovieById(int movieId) {
+			// TODO Auto-generated method stub
+			return movieDao.findById(movieId).get();
+		}
+	 
+		@Override
+		public List<Movie> getAllMovies() {
+			// TODO Auto-generated method stub
+			return movieDao.findAll();
+		}
+	 
+		@Override
+		public Movie saveMovieDetails(Movie movie) {
+			// TODO Auto-generated method stub
+			return movieDao.save(movie);
+		}
+	 
+		@Override
+		public List<Movie> saveMultipleMovieDetails(List<Movie> movieList) {
+			// TODO Auto-generated method stub
+//			for(Movie movie : movieList) {
+//				movieDao.save(movie);
+//			}
+			return movieDao.saveAll(movieList);
+		}
+	 
+		@Override
+		public Movie updateMovieDetails(Movie movie) {
+			// TODO Auto-generated method stub
+			Movie existingMovie = getMovieById(movie.getMovieId());
+			existingMovie.setCoverPhotoUrl(movie.getCoverPhotoUrl());
+			existingMovie.setDuration(movie.getDuration());
+			existingMovie.setMovieDescription(movie.getMovieDescription());
+			existingMovie.setMovieId(movie.getMovieId());
+			existingMovie.setMovieName(movie.getMovieName());
+			existingMovie.setReleaseDate(movie.getReleaseDate());
+			existingMovie.setTrailerUrl(movie.getTrailerUrl());
+			return movieDao.save(existingMovie);
+		}
+	 
+		@Override
+		public boolean deleteMovie(int movieId) {
+			// TODO Auto-generated method stub
+			Movie existingMovie = getMovieById(movieId);
+			if(existingMovie == null) {
+				return false;
+			}
+			else {
+			movieDao.deleteById(movieId);
+			return true;
+			}
+		}
+	 
+		@Override
+		public List<Movie> getMoviesByGenre(String genre) {
+			// TODO Auto-generated method stub
+			List<Movie> movies=movieDao.getByGenre(genre);
+			movies.stream().filter(list->list.getGenre().equals(genre)).collect(Collectors.toList());
+			return movies;
+		}
+} 
